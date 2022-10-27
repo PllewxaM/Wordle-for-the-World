1978b337ed2c1aee6e742cf6d602c2410bf28bb5
Summer Martin <martis36@tcnj.edu>
2022-10-27 13:26:34 -0400

    Adds basic wordle screen

---
+++ src/main.py
@@ -0,0 +1,13 @@
+# from turtle import Screen
+# import pygame
+# import sys
+# import random
+# from words import *
+
+# pygame.init()
+
+# # Constants
+
+# WIDTH, HEIGHT = 633, 900
+
+# SCREEN = pygame.display.set_mode((WIDTH, HEIGHT))
