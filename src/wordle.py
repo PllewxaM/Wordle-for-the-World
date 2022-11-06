aac6f0bac981c2fc407c57ec08e9af728a1c3a82
PllewxaM <parronm1@tcnj.edu>
2022-11-06 10:59:02 -0500

    Adds beginning of audio interface integration.

--- src/wordle.py
+++ src/wordle.py
@@ -3,15 +3,41 @@ import sys
 import random
 import words
 from words import *
+from gtts import gTTS
+import speech_recognition as sr
+import os
+from playsound import playsound
+import time
 
 pygame.init()
+
+# Audio Interface Initializers
+r = sr.Recognizer()
+mic = sr.Microphone()
+audio_interface_enabled = 0
+
+# Adjust based on current environment, start high and reduce
+# until good results found, good values between 50 and 4000
+r.energy_threshold = 4000
+
+# Text-to-speech languages: English, Spanish, French
+languages = ['en', 'es', 'fr']
+current_language = 0
+
+# Long sections of text used for instructing hands-free user
+startup = "Welcome to wordle for the world, if you need help, say, tutorial"
+tutorial = "Hello and welcome to wordle for the world. Im here to help you learn the commands to " \
+           "play the game in hands free mode. To hold onto a letter you may want to guess later, say " \
+           "the keyword, stash, followed by the letter you would like to hold onto. To hear your " \
+           "current guess, say the command, read guess. To submit your current guess, say the keyword, " \
+           "submit."
 
 # COLORS
 GREEN = "#6aaa64"
 YELLOW = "#c9b458"
 GREY = "#787c7e"
-WHITE = (255,255,255)
-BLACK = (0,0,0)
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
 OUTLINE = "#d3d6da"
 FILLED_OUTLINE = "#878a8c"
 
@@ -50,6 +76,46 @@ current_letter_bg_x = 175
 
 game_result = ""
 
+
+# Uses gTTS to say the string 'response' in language 'language'
+def say(response, language):
+    obj = gTTS(text=response, lang=language, slow=False)
+    obj.save("audio.mp3")
+    os.system("mpg123 audio.mp3")
+
+
+# Uses SpeechRecognition to translate a user response to text. Returns text
+def listen():
+    with mic as source:
+        audio = r.listen(source)
+
+    cur_text = r.recognize_google(audio)
+    return cur_text
+
+
+def say_by_char(response, language):
+    chars = [*response]
+    for c in chars:
+        say(c, language)
+        time.sleep(0.1)
+
+
+def say_and_confirm_by_char(guess, correct, language):
+    chars = [*guess]
+    correct = [*correct]
+    correct_index = 0
+    for c in chars:
+        say(c, language)
+        time.sleep(0.1)
+        if c == correct[correct_index]:
+            playsound('sound_effects/CorrectCharacter.wav')
+        elif c in correct:
+            playsound('sound_effects/SemiCorrectCharacter.wav')
+        else:
+            playsound('sound_effects/IncorrectCharacter.wav')
+        correct_index = correct_index + 1
+
+
 # draw squares
 def draw():
     for col in range(0, 5):
@@ -57,6 +123,7 @@ def draw():
             pygame.draw.rect(SCREEN, BLACK, [col * 65 + 175, row * 70 + 10, 60, 60], 1, 1)
             piece_text = FONT.render(BOARD[row][col], True, GREY)
             SCREEN.blit(piece_text, (col * 100 + 10, row * 100 + 15))
+
 
 # draw keyboard
 class Indicator:
@@ -76,6 +143,8 @@ class Indicator:
         SCREEN.blit(self.text_surface, self.text_rect)
         pygame.display.update()
 
+
+# ?
 indicator_x, indicator_y = 45, 450
 
 for i in range(3):
@@ -119,6 +188,7 @@ class Letter:
         pygame.draw.rect(SCREEN, "white", self.bg_rect)
         pygame.draw.rect(SCREEN, OUTLINE, self.bg_rect, 3)
         pygame.display.update()
+
 
 def check_guess(guess_to_check):
     # Goes through each letter and checks if it should be green, yellow, or grey.
@@ -165,6 +235,7 @@ def check_guess(guess_to_check):
     if guesses_count == 6 and game_result == "":
         game_result = "L"
 
+
 def lose_play_again():
     # Puts the play again text on the screen.
     pygame.draw.rect(SCREEN, "red", (10, 10, 680, 730))
@@ -176,6 +247,7 @@ def lose_play_again():
     SCREEN.blit(word_was_text, word_was_rect)
     SCREEN.blit(play_again_text, play_again_rect)
     pygame.display.update()
+
 
 def correct_play_again():
     # Puts the play again text on the screen.
@@ -193,6 +265,7 @@ def correct_play_again():
     SCREEN.blit(play_again_text, play_again_rect)
     pygame.display.update()
 
+
 def reset():
     # Resets all global variables to their default states.
     global guesses_count, CORRECT_WORD, guesses, current_guess, current_guess_string, game_result
@@ -208,6 +281,7 @@ def reset():
         indicator.bg_color = OUTLINE
         indicator.draw()
 
+
 def create_new_letter():
     # Creates a new letter and adds it to the guess.
     global current_guess_string, current_letter_bg_x
@@ -220,6 +294,7 @@ def create_new_letter():
         for letter in guess:
             letter.draw()
 
+
 def delete_letter():
     # Deletes the last letter from the guess.
     global current_guess_string, current_letter_bg_x
@@ -229,12 +304,80 @@ def delete_letter():
     current_guess.pop()
     current_letter_bg_x -= LETTER_X_SPACING
 
-while True:
+
+while not audio_interface_enabled:
     draw()
     if game_result == "L":
         lose_play_again()
     if game_result == "W":
         correct_play_again()
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_RETURN:
+                if game_result != "":
+                    reset()
+                else:
+                    if len(current_guess_string) == 5 and current_guess_string.lower() in WORDS:
+                        check_guess(current_guess)
+            elif event.key == pygame.K_BACKSPACE:
+                if len(current_guess_string) > 0:
+                    delete_letter()
+            else:
+                key_pressed = event.unicode.upper()
+                if key_pressed in "QWERTYUIOPASDFGHJKLZXCVBNM" and key_pressed != "":
+                    if len(current_guess_string) < 5:
+                        create_new_letter()
+
+    pygame.display.flip()
+
+while audio_interface_enabled:
+    draw()
+    if game_result == "L":
+        lose_play_again()
+    if game_result == "W":
+        correct_play_again()
+
+    try:
+        command = listen()
+        print(command)
+
+        if "tutorial" in command:  # Starts tutorial
+            say(tutorial, languages[current_language])
+        elif "stash" in command:  # Places character(s) into current guess
+            say("stash", languages[current_language])
+        elif "delete" in command:  # Deletes all characters from stash
+            say("delete", languages[current_language])
+        elif "read" in command:
+            if "guess" in command or "gas" in command or "guest" in command:
+                if "one" in command or "won" in command or "1" in command:
+                    say("read first guess", languages[current_language])
+                elif "two" in command or "to" in command or "2" in command or "too" in command:
+                    say("read second guess", languages[current_language])
+                elif "three" in command or "3" in command:
+                    say("read third guess", languages[current_language])
+                elif "four" in command or "for" in command or "4" in command:
+                    say("read fourth guess", languages[current_language])
+                elif "five" in command or "5" in command:
+                    say("read fifth guess", languages[current_language])
+                else:
+                    say("Read current guess not yet submitted", languages[current_language])
+            elif "semi" in command:
+                say("read semi correct guesses by character", languages[current_language])
+            elif "wrong" in command:
+                say("read incorrect guesses by character", languages[current_language])
+            else:
+                say("invalid command", languages[current_language])
+        elif "submit" in command:
+            say("submit", languages[current_language])
+        else:
+            say("invalid command", languages[current_language])
+
+    except Exception as e:
+        print("exception: " + repr(e))
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
