1386b6ddab9e28926dbd6e57a2691ab74b2965c9
Summer Martin <martis36@tcnj.edu>
2022-12-03 17:22:31 -0500

    Fixes high contrast mode colors

--- src/constants.py
+++ src/constants.py
@@ -36,9 +36,9 @@ BLACK = "#000000"
 MENU_COLOR = "#89a3b0"
 
 # high contract mode colors
-HIGH_CONTRAST_1 = "#f803fc"
-HIGH_CONTRAST_2 = "#03f0fc"
-HIGH_CONTRAST_3 = "#fc4503"
+HIGH_CONTRAST_1 = "#0C7BDC"
+HIGH_CONTRAST_2 = "#FFC20A"
+HIGH_CONTRAST_3 = GREY
 
 # colors used in color menu
 COLORS = [[RED, REDORANGE, ORANGE, ORANGEYELLOW], 
